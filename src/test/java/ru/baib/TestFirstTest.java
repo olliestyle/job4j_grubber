8db7753efdb63cfb4d2d25b8d6d139af8182577e
olliestyle <olliestyle1989@gmail.com>
2021-01-23 14:25:27 +0300

    Travis&Codecov_integration1

--- src/test/java/ru/baib/TestFirstTest.java
+++ src/test/java/ru/baib/TestFirstTest.java
@@ -12,4 +12,8 @@ public class TestFirstTest {
         assertThat(TestFirst.doPlusOne(4), is(5));
     }
 
+    @Test
+    public void simpleSecondTest() {
+        assertThat(TestFirst.doPlusOne(5), is(6));
+    }
 }
