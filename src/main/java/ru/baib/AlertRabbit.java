abe408d17e324dc96cfafa422fc56e1e32f59e7a
olliestyle <olliestyle1989@gmail.com>
2021-01-25 13:49:09 +0300

    1. Quartz[175122#303316]

---
+++ src/main/java/ru/baib/AlertRabbit.java
@@ -0,0 +1,51 @@
+package ru.baib;
+
+import org.quartz.*;
+import org.quartz.impl.StdSchedulerFactory;
+
+import java.io.InputStream;
+import java.util.Properties;
+
+import static org.quartz.JobBuilder.*;
+import static org.quartz.TriggerBuilder.*;
+import static org.quartz.SimpleScheduleBuilder.*;
+
+public class AlertRabbit {
+    public static void main(String[] args) {
+        try (InputStream in = AlertRabbit.class.getClassLoader().getResourceAsStream("rabbit.properties")) {
+            Properties prop = new Properties();
+            prop.load(in);
+            Integer period = Integer.parseInt(prop.getProperty("rabbit.interval"));
+            /* 1. Конфигурирование
+            Начало работы происходит с создания класса управляющего всеми работами.
+            В объект Scheduler мы будем добавлять задачи, которые хотим выполнять периодически.*/
+            Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
+            scheduler.start();
+            /* 2. Создание задачи
+            quartz каждый раз создает объект с типом org.quartz.Job. Вам нужно создать класс реализующий этот интерфейс.
+            Внутри этого класса нужно описать требуемые действия. В нашем случае - это вывод на консоль текста.*/
+            JobDetail job = newJob(Rabbit.class).build();
+            // 3. Создание расписания
+            SimpleScheduleBuilder times = simpleSchedule()
+                    .withIntervalInSeconds(period)
+                    .repeatForever();
+            /* 4. Задача выполняется через триггер
+            Здесь можно указать, когда начинать запуск. Мы хотим сделать это сразу.*/
+            Trigger trigger = newTrigger()
+                    .startNow()
+                    .withSchedule(times)
+                    .build();
+            // 5. Загрузка задачи и триггера в планировщик
+            scheduler.scheduleJob(job, trigger);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static class Rabbit implements Job {
+        @Override
+        public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
+            System.out.println("Rabbit runs here ...");
+        }
+    }
+}
