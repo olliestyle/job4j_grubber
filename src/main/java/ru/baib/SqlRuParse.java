476c032d5f76c2928215e22e75335386c59035a8
olliestyle <olliestyle1989@gmail.com>
2021-01-29 13:41:04 +0300

    2.1.1. Парсинг https://www.sql.ru/forum/job-offers/3[285210#303322]

--- src/main/java/ru/baib/SqlRuParse.java
+++ src/main/java/ru/baib/SqlRuParse.java
@@ -4,24 +4,44 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+
+import java.util.ArrayList;
 import java.util.Iterator;
 
 public class SqlRuParse {
+
+    ArrayList<String> links = new ArrayList<>();
+
+    public SqlRuParse() {
+        links.add("https://www.sql.ru/forum/job-offers");
+        links.add("https://www.sql.ru/forum/job-offers/2");
+        links.add("https://www.sql.ru/forum/job-offers/3");
+        links.add("https://www.sql.ru/forum/job-offers/4");
+        links.add("https://www.sql.ru/forum/job-offers/5");
+    }
+
+    public ArrayList<String> getLinks() {
+        return this.links;
+    }
+
     public static void main(String[] args) throws Exception {
-        Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers").get();
-        Elements row = doc.select(".postslisttopic");
-        Elements dates = doc.select(".altCol");
-        Iterator<Element> itRow = row.iterator();
-        Iterator<Element> itDates = dates.iterator();
-        itDates.next(); // у аттрибута altCol два значения, нам нужно только второе, где дата
-        while (itRow.hasNext()) {
-            Element href = itRow.next().child(0);
-            System.out.println(href.attr("href"));
-            System.out.println(href.text());
-            Element date = itDates.next();
-            System.out.println(date.text());
-            if (itDates.hasNext()) {
-                itDates.next();
+        SqlRuParse sqlRuParse = new SqlRuParse();
+        for (String str: sqlRuParse.getLinks()) {
+            Document doc = Jsoup.connect(str).get();
+            Elements row = doc.select(".postslisttopic");
+            Elements dates = doc.select(".altCol");
+            Iterator<Element> itRow = row.iterator();
+            Iterator<Element> itDates = dates.iterator();
+            itDates.next(); // у аттрибута altCol два значения, нам нужно только второе, где дата
+            while (itRow.hasNext()) {
+                Element href = itRow.next().child(0);
+                System.out.println(href.attr("href"));
+                System.out.println(href.text());
+                Element date = itDates.next();
+                System.out.println(date.text());
+                if (itDates.hasNext()) {
+                    itDates.next();
+                }
             }
         }
     }
