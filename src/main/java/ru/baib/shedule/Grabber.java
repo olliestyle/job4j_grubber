d4b12cd5e6d94f12d8247fff8f16bcc2c07f7019
olliestyle <olliestyle1989@gmail.com>
2021-02-15 14:29:03 +0300

    7. Web.[289478#303328]

--- src/main/java/ru/baib/shedule/Grabber.java
+++ src/main/java/ru/baib/shedule/Grabber.java
@@ -10,6 +10,9 @@ import ru.baib.store.Store;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
@@ -83,11 +86,33 @@ public class Grabber implements Grab {
         }
     }
 
+    public void web(Store store) {
+        new Thread(() -> {
+            try (ServerSocket server = new ServerSocket(Integer.parseInt(cfg.getProperty("port")))) {
+                while (!server.isClosed()) {
+                    Socket socket = server.accept();
+                    try (OutputStream out = socket.getOutputStream()) {
+                        out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+                        for (Post post : store.getAll()) {
+                            out.write(post.toString().getBytes("windows-1251"));
+                            out.write(System.lineSeparator().getBytes("windows-1251"));
+                        }
+                    } catch (IOException io) {
+                        io.printStackTrace();
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }).start();
+    }
+
     public static void main(String[] args) throws Exception {
         Grabber grabber = new Grabber();
         grabber.setCfg();
         Scheduler scheduler = grabber.scheduler();
         Store store = grabber.store();
         grabber.init(new SqlRuParse(), store, scheduler);
+        grabber.web(store);
     }
 }
